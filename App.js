25244fba298def00d60d848c136e4cfad7f142a1
iragaki jc <jcgroup.vni@gmail.com>
2025-11-21 17:58:18 +0700

    </>

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Day4 from './japonCPN/Day4';
+
+function App() {
+  return (
+    <div className="App">
+     <Day4></Day4>
+    </div>
+  );
+}
+
+export default App;
---
+++ japonCPN/Day4.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Day4 = () => {
+  return (
+    <div>
+      <div className="hiahia">
+        びょが　: render <br></br>
+        さいびょが : re render <br></br>
+        きょどう : xu ly hanh vi ui <br></br>
+        じょたい　かんり : state mangement <br></br>
+        ひどうき　しょり : xu ly async <br></br>
+        いぞん　ライブラリ : dpc library <br></br>
+        ぶんかつ : split cpn <br></br>
+        れｃ　か : suy giam hieu nang <br></br>
+        さいてきか :render optimatioz <br></br>
+        かそうドム : vitual doom <br></br>
+        <p className="lyrics">
+          <span>びょが　： render </span>
+          <span>さいびょが : redenr </span>
+          <span>きょどう : hanh vi UI </span>
+          <span> じょたい　かんり : state mng </span>
+          <span>ひどうき　しょり : xu ly async</span>
+          <span>いぞん　ライブラリ : dependencies librfary</span>
+
+          <span>ぶんかつ : split cpn</span>
+          <span>れっか : giảm performance </span>
+
+          <span>かそうドム : vitrual doom</span>
+          <span>さいてきか : render optimizatio　。。。</span>
+        </p>
+      </div>
+      <div className="hiahia2 marquee">
+        <span>iragaki x ivanila</span>
+      </div>
+    </div>
+  );
+};
+
+export default Day4;
